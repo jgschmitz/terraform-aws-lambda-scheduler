f9bfd5b2a3f953db970251ddeb6f588e6ef89a39
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2020-12-31 08:32:12 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 #fixed cloudwatch event rule - 
-#fixed cloudwatch event rule - "advanced"
+#fixed cloudwatch event rule 
 # Cloudwatch event rule
 resource "aws_cloudwatch_event_rule" "check-scheduler-event" {
   name                = "${var.resource_name_prefix}check-scheduler-event"
