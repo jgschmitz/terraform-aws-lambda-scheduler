e62e610230960e67b621ae4a2e0dc8acf10e522c
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2021-03-21 15:06:37 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,3 @@
-print 1,
-
 variable "schedule_expression" {
   default     = "cron(5 * * * ? *)"
   description = "the aws cloudwatch event rule scheule expression that specifies when the scheduler runs. Default is 5 minuts past the hour. for debugging use 'rate(5 minutes)'. See https://docs.aws.amazon.com/AmazonCloudWatch/latest/events/ScheduledEvents.html"
